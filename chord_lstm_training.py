8299213748cd38ceedcf07bfc34b6f67c0e2150c
brunnergino <brunnergino@users.noreply.github.com>
2018-04-11 10:24:07 +0200

    Update chord_lstm_training.py

--- chord_lstm_training.py
+++ chord_lstm_training.py
@@ -1,11 +1,12 @@
-# Author: Jonas Wiesendanger wjonas@student.ethz.ch
+# Author: Jonas Wiesendanger, Andres Konrad, Gino Brunner (brunnegi@ethz.ch)
 from settings import *
 from keras.models import Sequential
-from keras.layers.recurrent import LSTM
+from keras.layers import LSTM
 from keras.layers import Dense, Activation
-from keras.layers.embeddings import Embedding
+from keras.layers import Embedding
 from keras.optimizers import RMSprop, Adam
-from keras.utils import to_categorical
+import keras.utils
+from keras.utils import np_utils
 from keras.layers.wrappers import Bidirectional
 from random import shuffle
 import progressbar
@@ -16,40 +17,43 @@ import numpy as np
 import _pickle as pickle
 import os
 import data_class
+import time
 
 import tensorflow as tf
 from keras.backend.tensorflow_backend import set_session
 
 # Uncomment next block if you only want to use a fraction of the GPU memory:
 
-#config = tf.ConfigProto()
-#config.gpu_options.per_process_gpu_memory_fraction = 0.5
-#set_session(tf.Session(config=config))
+# config = tf.ConfigProto()
+# config.gpu_options.visible_device_list = "2"
+# config.gpu_options.per_process_gpu_memory_fraction = 0.9
+# set_session(tf.Session(config=config))
 
 #Path where the models are saved:
 model_path = 'models/chords/'
-model_filetype = '.h5'
+model_filetype = '.pickle'
 
 epochs = 20
-train_set_size = 30
-test_set_size = 5
-test_step = 30          # Calculate error for test set every this many songs
+train_set_size = 4
+test_set_size = 1
+test_step = 800        # Calculate error for test set every this many songs
 
 verbose = False
 show_plot = False
 save_plot = True
-lstm_size = 256
+lstm_size = 512
 batch_size = 1
 learning_rate = 0.00001
 step_size = 1
-save_step = 1
+save_step = 10
 shuffle_train_set = True
 bidirectional = False
 optimizer = 'Adam'
 
 fd = {'shifted': shifted, 'lr': learning_rate, 'emdim': chord_embedding_dim, 'opt': optimizer,
-'bi': bidirectional, 'lstms': lstm_size, 'trainsize': train_set_size, 'testsize': test_set_size}
-model_name = 'Shifted_%(shifted)s_Lr_%(lr)s_EmDim_%(emdim)s_opt_%(opt)s_bi_%(bi)s_lstmsize_%(lstms)s_trainsize_%(trainsize)s_testsize_%(testsize)s' % fd
+'bi': bidirectional, 'lstms': lstm_size, 'trainsize': train_set_size, 'testsize': test_set_size, 'samples_per_bar': samples_per_bar}
+t = str(int(round(time.time())))
+model_name = t+ '-Shifted_%(shifted)s_Lr_%(lr)s_EmDim_%(emdim)s_opt_%(opt)s_bi_%(bi)s_lstmsize_%(lstms)s_trainsize_%(trainsize)s_testsize_%(testsize)s_samples_per_bar%(samples_per_bar)s' % fd
 
 model_path = model_path + model_name + '/'
 if not os.path.exists(model_path):
@@ -61,9 +65,12 @@ print('loading data...')
 train_set, test_set = data_class.get_chord_train_and_test_set(train_set_size, test_set_size)
 print('creating model...')
 model = Sequential()
-model.add(Embedding(num_chords, chord_embedding_dim, batch_size=1, input_length=1))
-if bidirectional: model.add(Bidirectional(LSTM(lstm_size, stateful=True)))
-else: model.add(LSTM(lstm_size, stateful=True))
+model.add(Embedding(num_chords, chord_embedding_dim, input_length=step_size, name="embedding", batch_input_shape=(batch_size,step_size)))
+# model.add(Embedding(num_chords, chord_embedding_dim, input_length=step_size))
+
+# if bidirectional: model.add(Bidirectional(LSTM(lstm_size, stateful=True)))
+# else: model.add(LSTM(lstm_size, stateful=True))
+model.add(LSTM(lstm_size, stateful=True))
 model.add(Dense(num_chords))
 model.add(Activation('softmax'))
 if optimizer == 'Adam':
@@ -71,6 +78,7 @@ if optimizer == 'Adam':
 elif optimizer == 'RMS':
     optimizer = RMSprop(lr=learning_rate)
 loss = 'categorical_crossentropy'
+print("compiling model")
 model.compile(optimizer, loss)
 
 
@@ -82,14 +90,14 @@ def test():
     print('\nTesting:')
     total_test_loss = 0
 
-    bar = progressbar.ProgressBar(max_value=test_set_size, redirect_stdout=False)
+    bar = progressbar.ProgressBar(maxval=test_set_size, redirect_stdout=False)
     for i, test_song in enumerate(test_set):
         X_test = test_song[:-1]
-        Y_test = to_categorical(test_song[1:], num_classes=num_chords)
+        Y_test = np_utils.to_categorical(test_song[1:], num_classes=num_chords)
         loss = model.evaluate(X_test, Y_test, batch_size=batch_size, verbose=verbose)
         model.reset_states()
         total_test_loss += loss
-        bar.update(i)
+        bar.update(i+1)
     total_test_loss_array.append(total_test_loss/test_set_size)
     print('\nTotal test loss: ', total_test_loss/test_set_size)
     print('-'*50)
@@ -111,13 +119,14 @@ def train():
         print('Epoch ', e, 'of ', epochs, 'Epochs\nTraining:')
         if shuffle_train_set:
             shuffle(train_set)
-        bar = progressbar.ProgressBar(max_value=train_set_size)
+        bar = progressbar.ProgressBar(maxval=train_set_size)
         for i, song in enumerate(train_set):
+            # bar.start()
             X = song[:-1]
-            Y = to_categorical(song[1:], num_classes=num_chords)
-            hist = model.fit(X, Y, batch_size=batch_size, shuffle=False, epochs=1, verbose=verbose)
+            Y = np_utils.to_categorical(song[1:], num_classes=num_chords)
+            hist = model.fit(X, Y, batch_size=batch_size, shuffle=False, verbose=verbose)
             model.reset_states()
-            bar.update(i)
+            bar.update(i+1)
 #            print(hist.history)
             total_train_loss += hist.history['loss'][0]
             if (i+1)%test_step is 0:
@@ -128,7 +137,7 @@ def train():
     
         if e%save_step is 0:
             print('saving model')
-            model_save_path = model_path + 'model_' + 'Epoch' + str(e) + model_filetype
+            model_save_path = model_path + 'model_' + 'Epoch' + str(e) + '_' + str(i+1) + model_filetype
             model.save(model_save_path)
 
 def save_params():
@@ -139,12 +148,14 @@ def save_params():
         text_file.write("lstm_size: %s" % lstm_size + '\n')
         text_file.write("embedding_dim: %s" % chord_embedding_dim + '\n')
         text_file.write("learning_rate: %s" % learning_rate + '\n')
-        text_file.write("save_step: %s" % save_step + '\n')
+        #text_file.write("save_step: %s" % save_step + '\n')
         text_file.write("shuffle_train_set: %s" % shuffle_train_set + '\n')
         text_file.write("test_step: %s" % test_step + '\n')
         text_file.write("bidirectional: %s" % bidirectional + '\n')
         text_file.write("num_chords: %s" % num_chords + '\n')
         text_file.write("chord_n: %s" % chord_n + '\n')
 
+print("saving params")
 save_params()
+print("starting training..")
 train()
